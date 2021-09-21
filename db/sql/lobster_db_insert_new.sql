74a85862bf2c01cc16180d0a0a3e14b34f520c7f
Thomas Langlois <thomas.langlois@mac.com>
2021-09-21 15:56:24 +0000

    tweak

--- db/sql/lobster_db_insert_new.sql
+++ db/sql/lobster_db_insert_new.sql
@@ -1,1 +1,1 @@
-insert into lobster (time, lobster_counter, lobster_votes) values(#TIME, #LOBSTER_COUNTER, #LOBSTER_VOTES);
+insert into lobster (time, lobster_counter, lobster_votes) values("#TIME", #LOBSTER_COUNTER, #LOBSTER_VOTES);
