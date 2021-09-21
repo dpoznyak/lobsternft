8f18603433c36c3c5ba617f1edb349493b5589fc
Thomas Langlois <thomas.langlois@mac.com>
2021-09-21 08:58:11 +0100

    tweak

--- db/scripts/lobsternft_get_last_json_from_db.sh
+++ db/scripts/lobsternft_get_last_json_from_db.sh
@@ -6,10 +6,10 @@ SQL_DIR="$DB_DIR/sql"
 
 LAST=$(cat $SQL_DIR/lobster_db_select_last.sql | sqlite3 $DB_DIR/lobster.db)
 
-ID=$(awk -F"!" '{print $1}')
-TIME=$(awk -F"!" '{print $2}')
-LOBSTER_COUNTER=$(awk -F"!" '{print $3}')
-LOBSTER_VOTES=$(awk -F"!" '{print $4}')
+ID=$(awk -F"¦" '{print $1}')
+TIME=$(awk -F"¦" '{print $2}')
+LOBSTER_COUNTER=$(awk -F"¦" '{print $3}')
+LOBSTER_VOTES=$(awk -F"¦" '{print $4}')
 
 $(cat <<-END > ./tmp.json
 {
