231747183066154f955c2b8745c4f34a0a497b2a
Thomas Langlois <thomas.langlois@mac.com>
2021-09-21 08:43:48 +0100

    tweaks

--- db/scripts/lobsternft_get_last_json_from_db.sh
+++ db/scripts/lobsternft_get_last_json_from_db.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 SQL_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
 
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+echo "SQL_DIR: $SQL_DIR"
+
 LAST=$(cat $SQL_DIR/lobster_db_select_last.sql | sqlite3 lobster.db)
 echo "LAST: $LAST"
